7e202bee34897cc72bdac3cb806ba44e0f6c2250
Rayablepy <rayhan11.0808@gmail.com>
2026-07-18 18:19:34 +0800

    added create_agent harness

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+
 from config import CHAT_MODEL_NAME
 from loader import query_data
 from langchain.chat_models import init_chat_model
+from langchain.agents import create_agent
+tools = [query_data]
 model=init_chat_model(
     model=CHAT_MODEL_NAME,
     model_provider="openai",
     base_url="http://localhost:1234/v1",
     api_key="not-needed",
     temperature=0.5,
-).bind_tools([query_data])
-
+)
+agent = create_agent(
+    model=model,
+    tools=tools
+)
 def getresponse(user:str) -> str:
-    response = model.invoke(user)
+    response = agent.invoke(user)
     return response.content
